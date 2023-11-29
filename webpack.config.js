88b31a38e6f3d3322ddd8fedd9ce092e272717b8
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-11-29 00:57:47 -0500

    initial webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        index: "./src/index.js",
+        event: "./src/event.js",
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            title: "Todo List",
+        }),
+    ],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    optimization: {
+        runtimeChunk: "single",
+    },
+};
