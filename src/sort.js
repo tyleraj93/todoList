13caac7577ba85a39c30d0668b444f0986577d3c
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-12-05 19:20:04 -0500

    Start and export sort feature

---
+++ src/sort.js
@@ -0,0 +1,7 @@
+// I build the sidebar with buttons to sort by
+// today, this week, this month, all projects,
+// and completed
+
+export default function helloWorldSort() {
+    console.log("hello from sort.js");
+}
