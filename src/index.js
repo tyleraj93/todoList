d423526f3bb1aeab4d20375ac19e41ccd41c50c1
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-12-05 19:18:09 -0500

    Add checks for importing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import helloWorld from "./event.js";
+import helloWorldEvent from "./event.js";
+import helloWorldSort from "./sort.js";
+import helloWorldToDo from "./todo.js";
+import helloWorldMonthly from "./monthly.js";
 
 const content = document.getElementById("content");
 
-helloWorld();
+helloWorldEvent();
+helloWorldSort();
+helloWorldToDo();
+helloWorldMonthly();
