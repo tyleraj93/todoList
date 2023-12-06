ac47ba0e81da2c86772eac2e624542f6626a7aae
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-12-05 19:19:26 -0500

    Add class for event

--- src/event.js
+++ src/event.js
@@ -1,3 +1,41 @@
-export default function helloWorld() {
+// I build new events and event functions like add and delete
+
+import { NormalModule } from "webpack";
+
+export default function helloWorldEvent() {
     console.log("hello from event.js");
+}
+
+// Add a new event
+class Event {
+    constructor(name, date, priority, notes) {
+        this._name = name;
+        this._date = date;
+        this._priority = priority;
+        this._notes = notes;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get date() {
+        return this._date;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    get notes() {
+        return this._notes;
+    }
+}
+
+// Empty list to add events to
+const planner = [];
+
+const addEventButton = (location) => {
+    const addButton = document.createElement("button");
+    // addButton.addEventListener("click", () => )
 }
