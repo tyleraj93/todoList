4223ef0763dd59662006370304f520d41aece5a6
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-12-05 23:42:05 -0500

    Add imports and built modal

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import helloWorldEvent from "./event.js";
 import helloWorldSort from "./sort.js";
 import helloWorldToDo from "./todo.js";
 import helloWorldMonthly from "./monthly.js";
+import eventModal, { buildModalButton} from "./addEventModal.js";
 
 const content = document.getElementById("content");
+eventModal(content);
+
+const modal = document.querySelector(".dialogContainer");
+buildModalButton(content, modal);
 
 helloWorldEvent();
 helloWorldSort();
