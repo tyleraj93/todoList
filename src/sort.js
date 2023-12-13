9ed512e1e2ab8b0859c9761c79da459089ffa08b
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-12-13 00:14:32 -0500

    Add functions to sort by day, week, and month

--- src/sort.js
+++ src/sort.js
@@ -1,7 +1,83 @@
+import { retrieveLocalEvents } from "./localStorage";
+import { buildTodoEvent } from "./todo";
+import { isSameDay, isSameWeek, isSameMonth } from "date-fns";
 // I build the sidebar with buttons to sort by
 // today, this week, this month, all projects,
 // and completed
 
-export default function helloWorldSort() {
-    console.log("hello from sort.js");
+export default function buildSortSection(location) {
+    const sortSection = document.createElement("div");
+    sortSection.id = "sortSection";
+
+    const todoText = document.createElement("p");
+    todoText.textContent = "Todo";
+    todoText.classList.add("sidebar-text");
+    sortSection.appendChild(todoText);
+
+    const todayButton = document.createElement("button");
+    todayButton.textContent = "Today";
+    todayButton.classList.add("sidebar-text");
+    todayButton.addEventListener("click", () => sortToday());
+    sortSection.appendChild(todayButton);
+
+    const weekButton = document.createElement("button");
+    weekButton.textContent = "This week";
+    weekButton.classList.add("sidebar-text");
+    weekButton.addEventListener("click", () => sortWeek());
+    sortSection.appendChild(weekButton);
+
+    const monthButton = document.createElement("button");
+    monthButton.textContent = "This month";
+    monthButton.classList.add("sidebar-text");
+    monthButton.addEventListener("click", () => sortMonth());
+    sortSection.appendChild(monthButton);
+
+    location.appendChild(sortSection);
+}
+
+let today = new Date();
+let month = today.getMonth();
+let day = today.getDate();
+
+function clearTodoSection() {
+    const todoSection = document.getElementById("todoSection");
+    todoSection.textContent = "";
+}
+
+function isInRange(value, min, max) {
+    return value >= min && value <= max;
+}
+
+function sortToday() {
+    clearTodoSection();
+    const events = retrieveLocalEvents("events");
+    for (const event of events) {
+        const eventDay = new Date(event._date);
+        if (isSameDay(today, eventDay)) {
+            buildTodoEvent(event);
+        };
+    }
+};
+
+function sortWeek() {
+    clearTodoSection();
+    const events = retrieveLocalEvents("events");
+    for (const event of events) {
+        const eventDay = new Date(event._date);
+        if (isSameWeek(today, eventDay)) {
+            buildTodoEvent(event);
+        }
+    }
+}
+
+
+function sortMonth() {
+    clearTodoSection();
+    const events = retrieveLocalEvents("events");
+    for (const event of events) {
+        const eventDay = new Date(event._date);
+        if (isSameMonth(today, eventDay)) {
+            buildTodoEvent(event);
+        }
+    }
 }
