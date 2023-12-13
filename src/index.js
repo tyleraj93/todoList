944fd74454bced1426e4a9ea5782e31d28c1db32
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-12-13 00:17:21 -0500

    Fixed typo in todo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import helloWorldEvent from "./event.js";
 import buildSortSection from "./sort.js";
-import buildToDoSection from "./todo.js";
+import buildTodoSection from "./todo.js";
 import helloWorldMonthly from "./monthly.js";
 import buildEventModal, { buildModalButton } from "./addEventModal.js";
 
 const content = document.getElementById("content");
 buildEventModal(content);
 buildSortSection(content);
-buildToDoSection(content);
+buildTodoSection(content);
 
 const modal = document.querySelector("#dialogContainer");
 // Takes in placement and modal location
