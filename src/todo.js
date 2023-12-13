985eec8016ba84c6b30e79fc030701ce04c74f92
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-12-13 00:12:40 -0500

    Add function to build events

--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,40 @@
 // I display the selected projects in the range
 // selected from sort.
 
-export default function buildToDoSection(location) {
-    const toDoSection = document.createElement("div");
-    toDoSection.id = "toDoSection";
-    location.appendChild(toDoSection);
+export default function buildTodoSection(location) {
+    const todoSection = document.createElement("div");
+    todoSection.id = "todoSection";
+    location.appendChild(todoSection);
 }
+
+export function buildTodoEvent(eventInfo) {
+    console.log(eventInfo._name);
+    const todoSection = document.getElementById("todoSection");
+
+    const eventDiv = document.createElement("div");
+    eventDiv.classList.add("event-Div");
+    todoSection.appendChild(eventDiv);
+
+    const completeButton = document.createElement("button");
+    eventDiv.appendChild(completeButton);
+
+    const eventTitle = document.createElement("p");
+    eventTitle.classList.add("event-title");
+    eventTitle.textContent = `${eventInfo._name}`;
+    eventDiv.appendChild(eventTitle);
+
+    const eventDueDate = document.createElement("p");
+    eventDueDate.classList.add("event-due-date");
+    eventDueDate.textContent = `${eventInfo._date}`;
+    eventDiv.appendChild(eventDueDate);
+
+    const eventNotes = document.createElement("p");
+    eventNotes.classList.add("event-notes");
+    eventNotes.textContent = `${eventInfo._notes}`;
+    eventDiv.appendChild(eventNotes);
+
+    const eventPriority = document.createElement("p");
+    eventPriority.classList.add("event-priority");
+    eventPriority.textContent = `${eventInfo._priority}`;
+    eventDiv.appendChild(eventPriority);
+};
