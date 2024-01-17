2ca1868933fd6268fd0dab6f0422ba76e6ac6c05
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2024-01-16 22:16:35 -0500

    Remove unused imports and unnecessary doc location

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import helloWorldEvent from "./event.js";
 import buildSortSection from "./sort.js";
 import buildTodoSection from "./todo.js";
-import helloWorldMonthly from "./monthly.js";
 import buildEventModal from "./addEventModal.js";
 import "./style.css";
 
-const content = document.getElementById("content");
-buildEventModal(content);
-buildSortSection(content);
-buildTodoSection(content);
 
-helloWorldEvent();
-helloWorldSort();
-helloWorldToDo();
-helloWorldMonthly();
+buildEventModal();
+buildSortSection();
+buildTodoSection();
